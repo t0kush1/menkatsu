2222dfa039bc8f3946238768d23d4ca600d33078
tokushi_kudo <tokushi.kudo@rakus-partners.co.jp>
2025-03-29 13:26:40 +0900

    デモTOP画面作成、一部コンポーネント化

---
+++ src/components/Ramencard.tsx
@@ -0,0 +1,29 @@
+type RamenRecord = {
+  id: number;
+  shopName: string;
+  visitDate: string;
+  rating: number; // 1~5
+  comment: string;
+};
+
+type RamenRecordProps = {
+  record: RamenRecord;
+};
+
+export default function RamenCard({ record }: RamenRecordProps) {
+  return (
+    <li
+      style={{
+        marginBottom: "1.5rem",
+        border: "1px solid #ccc",
+        padding: "1rem",
+        borderRadius: "8px",
+      }}
+    >
+      <h3>{record.shopName}</h3>
+      <p>訪問日: {record.visitDate}</p>
+      <p>評価: {record.rating}</p>
+      <p>感想: {record.comment}</p>
+    </li>
+  );
+}
