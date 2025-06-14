0b8d50572dcae7eb6a2ff27edab04ec3b0b84575
tokushi_kudo <tokushi.kudo@rakus-partners.co.jp>
2025-06-14 16:22:34 +0900

    コメント追加

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
 import type { NextConfig } from 'next';
 import nextPWA from 'next-pwa';
 
+// Next.js設定を定義
 const baseConfig: NextConfig = {
   reactStrictMode: true,
 };
 
+// PWA設定を追加
 export default nextPWA({
   dest: 'public', 
   register: true,
